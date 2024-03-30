4ad773e1bda490c96970f091b62a114a9d989d8b
edelbarno <barnoedel12@gmail.com>
2024-03-30 10:54:46 +0300

    add arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Write your solution here!
+
+const cats = ["Milo","Otis","Garfield"];
+cats.length = 0;
+function destructivelyAppendCat(name){
+    cats.push("Ralph"); 
+    cats;
+}
+const cats = ["Milo","Otis","Garfield"];
+cats.length = 0;
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+    cats;
+}
+const cats = ["Milo","Otis","Garfield"];
+cats.length = 0;
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+const cats = ["Milo","Otis","Garfield"];
+cats.length = 0;
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+const cats = ["Milo","Otis","Garfield"];
+cats.length = 0;
+function appendCat(name){
+ cats.splice(3,0,"Broom");
+}
+function appendCat(name){
+ const newCats=["...cats",name];
+ return newCats;
+}
+const cats = ["Milo","Otis","Garfield"];
+cats.length = 0;
+function prependCat(name){
+    cats.push("Bob")
+}
+function prependCat(name){
+ const newCats=["name",...cats];
+ return newCats;
+}
+const cats = ["Milo","Otis","Garfield"];
+cats.length =0;
+function removeLastCat(){
+const newCats=cats.slice(-1);
+return newCats;
+}
+const cats = ["Milo","Otis","Garfield"];
+cats.length = 0;
+function removeFirstCat(){
+const newCats=cats.slice(1);
+return newCats;
+}
